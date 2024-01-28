4cf1f3656e8deb4239cb2e38e335d00786ad1f08
Wesley Fuchs <fuchswesley@hotmail.com>
2024-01-28 01:48:48 -0300

    adicionado txt com datas

---
+++ setup.py
@@ -0,0 +1,19 @@
+import sys
+from cx_Freeze import setup, Executable
+
+build_exe_options = {
+    "packages": [],
+    "include_files": ["assets/"],
+}
+
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"  # Use "Win32GUI" se você estiver criando uma GUI
+
+setup(
+    name="auto-sefaz-pi",
+    version="2.0",
+    description="Automatização de download de NF-e e NFC-e no site sefaz PI",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("auto-click.py", base=base)],
+)
